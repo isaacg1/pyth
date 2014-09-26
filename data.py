4f7c04d0e84a5e029762c2abf340147c456371b1
Isaac Grosof <isaacg@mit.edu>
2014-09-26 18:32:28 -0400

    Fixed memoization so it works with all inputs, inc. lists, sets, dicts.

--- data.py
+++ data.py
@@ -28,25 +28,19 @@ c_to_s = {
 import collections
 
 
+# memoizes function calls, key = repr of input.
 class memoized(object):
-    '''Decorator. Caches a function's return value each time it is called.
-    If called later with the same arguments, the cached value is returned
-    (not reevaluated).
-    '''
     def __init__(self, func):
         self.func = func
         self.cache = {}
 
     def __call__(self, *args):
-        if not isinstance(args, collections.Hashable):
-            # uncacheable. a list, for instance.
-            # better to not cache than blow up.
-            return self.func(*args)
-        if args in self.cache:
-            return self.cache[args]
+        args_repr = repr(args)
+        if args_repr in self.cache:
+            return self.cache[args_repr]
         else:
             value = self.func(*args)
-            self.cache[args] = value
+            self.cache[args_repr] = value
             return value
 
 
