048e38038d9713554a81e9256eebfa2c809d529c
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2014-12-31 23:56:17 +0100

    Add files for Heroku

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 from http.server import HTTPServer, CGIHTTPRequestHandler
+import sys
 
 class Handler(CGIHTTPRequestHandler):
     cgi_directories = ["/"]
 
-PORT = 8000
+PORT = int(sys.argv[1])
 
 httpd = HTTPServer(("", PORT), Handler)
 print("serving at port", PORT)
