540c73d33aa658502cf74f4234cf18796660423e
Isaac Grosof <isaacg@mit.edu>
2015-02-10 09:55:19 -0500

    Fixed bug with second reduce

--- data.py
+++ data.py
@@ -153,5 +153,5 @@ next_c_to_f = {
     'f': [('Pfilter(lambda Y:', 2), ('Pfilter(lambda Z:', 2), ],
     'm': [('Pmap(lambda k:', 2), ('Pmap(lambda b:', 2), ],
     'o': [('order(lambda Z:', 2), ],
-    'u': [('reduce(lambda N,T:', 2), ],
+    'u': [('reduce(lambda N,T:', 3), ],
     }
