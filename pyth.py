76afc606b9ef9f6901ba4e607cee6139a7758aa8
Isaac Grosof <isaacg@mit.edu>
2015-05-26 12:32:05 -0700

    Added R, right map, and V, vectorize.
    %R7[10 50 90 = [3, 1, 6]
    *V[1 2 3)[5 3 7) = [5, 6, 21]

--- pyth.py
+++ pyth.py
@@ -111,17 +111,25 @@ def parse(code, spacing="\n "):
         if active_char in c_to_f and not c_to_f[active_char][1] == 0:
             map_arg = c_to_f['m'][0][-2]
             arity = c_to_f[active_char][1]
+            remainder = rest_code[1:]
             if arity == 1:
-                return parse('m' +
-                             active_char +
-                             map_arg +
-                             rest_code[1:])
+                return parse('m' + active_char + map_arg + remainder)
             else:
-                return parse('m' +
-                             active_char +
-                             '.*' +
-                             map_arg +
-                             rest_code[1:])
+                return parse('m' + active_char + '.*' + map_arg + remainder)
+
+    # <binary function>R<any><seq>: Right Map operator
+    # '+R4[1 2 3 4' -> 'm+d4[1 2 3 4'.
+    if rest_code and rest_code[0] == 'R':
+        if (active_char in c_to_f and c_to_f[active_char][1] == 2) or\
+                (active_char in c_to_i and c_to_i[active_char][1] == 2):
+            map_arg = c_to_f['m'][0][-2]
+            return parse('m' + active_char + map_arg + rest_code[1:])
+
+    # <function>V<seq><seq> Vectorize operator.
+    # Equivalent to <func>MC,<seq><seq>.
+    if rest_code and rest_code[0] == 'V':
+        if active_char in c_to_f and 1 <= c_to_f[active_char][1] <= 2:
+            return parse(active_char + "MC," + rest_code[1:])
 
     # =<function/infix>: augmented assignment.
     if active_char == '=':
