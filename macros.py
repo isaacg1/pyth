5d7c0a125d4a3d73ca0c238b79ef4d45c4f5a68e
Isaac Grosof <isaacg@mit.edu>
2015-06-18 01:07:25 -0700

    Small bugfix to .w
    Leaving out second param was producing o..txt instead of o.txt

--- macros.py
+++ macros.py
@@ -302,18 +302,18 @@ def read_file(a):
         if any(a.lower().endswith("." + i) for i in ["png", "jpg", "jpeg", "gif", "svg", "ppm", "pgm", "pbm"]):
             img = Image.open(a)
             data = list(img.getdata())
-            
+
             #If alpha all 255, take out alpha
             if len(data[0])>3 and all(i[3]==255 for i in data):
                 data = [i[:3] for i in data]
-            
+
             #Check grayscale
             if all(i.count(i[0])==len(i) for i in data):
                 data = [i[0] for i in data]
-            
+
             data = chop(data, img.size[0])
             return data
-            
+
         if a.startswith("http"):
             b = urllib.request.urlopen(a)
         else:
@@ -321,7 +321,7 @@ def read_file(a):
 
         b = [lin[:-1] if lin[-1] == '\n' else lin for lin in b]
         return b
-        
+
 
     raise BadTypeCombinationError("'", a)
 environment['read_file'] = read_file
@@ -1458,15 +1458,15 @@ def Pwrite(a, b=''):
 
     if is_lst(a):
         suffix = suffix if suffix else 'png'
-        
+
         if not is_lst(a[0][0]):
             a = [(i, i, i) for i in a]
-        
+
         img =  Image.new("RGB" + ("A" if len(a[0][0])>3 else ""), (len(a[0]), len(a)))
         img.putdata(Psum(a))
         img.save(prefix + "." + suffix)
     else:
-        suffix = suffix if suffix else ".txt"
+        suffix = suffix if suffix else "txt"
 
         with open(prefix + '.' + suffix, 'a') as f:
             f.write(("\n".join(map(str, a)) if is_seq(a) and not isinstance(a, str)
