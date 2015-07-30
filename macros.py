385e66d32230deca907a89310161cd4f16973e80
Jakob Kogler <jakob.kogler@gmail.com>
2015-07-30 19:20:38 +0200

    Change token to .O

--- macros.py
+++ macros.py
@@ -1042,9 +1042,15 @@ def Pbin(a):
 environment['Pbin'] = Pbin
 
 
-# .O. int/str
+# .O. int/str. Octal, average
 def Poct(a):
-    return oct(int(hex_multitype(a, ".O"), 16))[2:]
+    if is_seq(a) and all(map(is_num, a)):
+        if len(a) == 0:
+            return 0.0
+        else:
+            return sum(a) / len(a)
+    else:
+        return oct(int(hex_multitype(a, ".O"), 16))[2:]
 environment['Poct'] = Poct
 
 
@@ -1465,17 +1471,6 @@ def reduce2(a, b):
         return acc
     raise BadTypeCombinationError(".U", a, b)
 environment['reduce2'] = reduce2
-
-
-# .v. average
-def average(a):
-    if is_seq(a) and all(map(is_num, a)):
-        if len(a) == 0:
-            return 0.0
-        else:
-            return sum(a) / len(a)
-    raise BadTypeCombinationError(".v", a)
-environment['average'] = average
 
 
 # .w. write
