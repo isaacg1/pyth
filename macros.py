5577bbbf13e1f366877b38020d454e3e07b7169c
Steven-Hewitt <steven.hewitt.cal@gmail.com>
2016-09-19 17:52:29 -0700

    Fixed types in deltas

--- macros.py
+++ macros.py
@@ -1954,7 +1954,7 @@ environment['remove'] = remove
 
 # .+. seq
 def deltas(a):
-    if is_col(a):
+    if is_seq(a):
         return [minus(a[i+1], x) for i,x in enumerate(a[:-1])]
     raise BadTypeCombinationError(".+", a)
 environment['deltas'] = deltas    
